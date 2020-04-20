a391838f43f53b7ad2631036b50fa53ec99fc4aa
StefanoSilv <silver.stefano@gmail.com>
2020-04-20 10:35:54 +0200

    added the functions in index.js

---
+++ index.js
@@ -0,0 +1,73 @@
+const axios = require("axios");
+
+module.exports = {
+  getByDate: date => {
+    axios
+      .get(`https://exchange-rates-library.herokuapp.com/rates?date=${date}`)
+      .then(res => {
+        return res.data;
+      })
+      .catch(err => {
+        return { err };
+      });
+  },
+  getByPastDays: number => {
+    let date = new Date();
+    let pastDate = new Date(
+      date.setDate(date.getDate() - number)
+    ).toISOString();
+    axios
+      .get(
+        `https://exchange-rates-library.herokuapp.com/rates?date=${pastDate
+          .toString()
+          .substr(0, 10)}`
+      )
+      .then(res => {
+        console.log(res.data);
+        return res.data;
+      })
+      .catch(err => {
+        return { err };
+      });
+  },
+  getPastMonth: () => {
+    let date = new Date();
+    let pastDate = new Date(date.setDate(date.getDate())).toISOString();
+    let year = pastDate.toString().substr(0, 5);
+    let month = (Number(pastDate.toString().substr(5, 2)) - 1)
+      .toString()
+      .padStart(2, "0");
+    let day = pastDate.toString().substr(7, 3);
+    let dateQuery = year + month + day;
+    axios
+      .get(
+        `https://exchange-rates-library.herokuapp.com/rates?date=${dateQuery}`
+      )
+      .then(res => {
+        console.log(res.data);
+        return res.data;
+      })
+      .catch(err => {
+        return { err };
+      });
+  },
+  getPastYear: () => {
+    let date = new Date();
+    let pastDate = new Date(date.setDate(date.getDate())).toISOString();
+    let year = (Number(pastDate.toString().substr(0, 4)) - 1).toString();
+    let month = pastDate.toString().substr(4, 3);
+    let day = pastDate.toString().substr(7, 3);
+    let dateQuery = year + month + day;
+    axios
+      .get(
+        `https://exchange-rates-library.herokuapp.com/rates?date=${dateQuery}`
+      )
+      .then(res => {
+        console.log(res.data);
+        return res.data;
+      })
+      .catch(err => {
+        return { err };
+      });
+  }
+};
